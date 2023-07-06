e55d57f4a8576e1234cf46a7415c834fdee6b964
MaksimKlim <milk.max4@gmail.com>
2023-07-06 15:26:15 +0200

    lesson 3 v4

---
+++ lesson3/src/main/java/Task5.java
@@ -0,0 +1,12 @@
+public class Task5 {
+    public static void main(String[] args) {
+
+        for (int i  = 1 ; i<100 ; i++ ){
+
+            System.out.println(i++);
+
+        }
+        System.out.println("END");
+        }
+    }
+
