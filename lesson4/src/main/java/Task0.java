e51c514c2934e3c1b6a3229315fdf50893cca5b0
MaksimKlim <milk.max4@gmail.com>
2023-07-07 17:20:36 +0200

    lesson 4 v1

---
+++ lesson4/src/main/java/Task0.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Task0 {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[] {10,4,15,6,45,1};
+
+        Arrays.sort(arr);
+        String arrText = Arrays.toString(arr);
+        System.out.println(arrText);
+
+    }
+}
