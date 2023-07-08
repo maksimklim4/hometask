7f40d06034649ce73bafa9e290111aa624639163
MaksimKlim <milk.max4@gmail.com>
2023-07-08 16:20:44 +0200

    lesson 4 task 2 v1

---
+++ lesson4/src/main/java/Task2.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("enter the number");
+        int n = userInput.nextInt();
+
+        double[] arr = new double[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Math.random();
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+        double max = arr[0];
+        double min = arr[0];
+        double allsum = arr[0];
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];}
+            if (arr[i] < max) {
+                max = arr[i];}
+
+allsum = allsum +
+    }
+}
+
