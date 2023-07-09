105a2410211507fabf87f9952e20a17146b3e019
MaksimKlim <milk.max4@gmail.com>
2023-07-09 12:12:34 +0200

    lesson 4 done without task 1 (copy)

--- lesson4/src/main/java/Task0.java
+++ lesson4/src/main/java/Task0.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 import java.util.Scanner;
-
+//0. Создайте массив целых чисел. Напишете программу, которая выводит
+//сообщение о том, входит ли заданное число в массив или нет.
+//Пусть число для поиска задается с консоли (класс Scanner).
 public class Task0 {
 
     public static void main(String[] args) {
