67142882cd3ecd405cd8af9396a3d2ce388d876b
MaksimKlim <milk.max4@gmail.com>
2023-07-09 11:08:48 +0200

    lesson 4 task 3 done

---
+++ lesson4/src/main/java/Task3.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+
+        int[] arr1 = new int[]{10, 4, 6, 45, 1};
+
+        int[] arr2 = new int[]{8, 5, 90, 23, 65};
+
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+
+        int n = 0;
+        int summa1 = 0;
+        int summa2 = 0;
+
+        for (int i = 0; i < arr1.length; i++) {
+            summa1 = summa1 + arr1[i];
+        }
+
+        int sr1 = summa1 / arr1.length;
+
+        for (int i = 0; i < arr2.length; i++) {
+            summa2 = summa2 + arr2[i];
+        }
+
+        int sr2 = summa2 / arr2.length;
+
+        System.out.println("sr1 " +  sr1);
+        System.out.println("sr2 " +  sr2);
+
+        if (sr1 > sr2){
+            System.out.println("Среднее больше в первом массиве");
+        }
+        if (sr1 < sr2){
+            System.out.println("Средне больше во втором массиве");
+        }
+        }
+    }
+
