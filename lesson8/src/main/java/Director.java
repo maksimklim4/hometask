d007482c0c87451cb92d2398bb09bacddc6f30c9
MaksimKlim <milk.max4@gmail.com>
2023-07-23 14:40:19 +0200

    lesson8 2 cd

--- lesson8/src/main/java/Director.java
+++ lesson8/src/main/java/Director.java
@@ -4,13 +4,15 @@ public class Director extends Employee{
     public Director(String name, String surname, int experience){
         super(name, surname, Position.DIRECTOR, experience);
     }
+//метод назначения
+    public void addSubordinates(Employe[] ) {
 
-    public void addSubordinates(int numbersSubordinates) {
-       this.numbersSubordinates = numbersSubordinates;
+        this.numbersSubordinates = numbersSubordinates;
     }
     public int getBaseSalary() {
         return baseSalary;
     }
+    @Override
     public double calculateSalary() {
        // double salary = getBaseSalary() * position.getCoefficient() * experience;
         // return salary;
--- lesson8/src/main/java/Main.java
+++ lesson8/src/main/java/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
         Employee worker1 = new Worker("Anton", "Smith", 2);
         Employee worker2 = new Worker("Roman", "Chan", 5);
         Employee worker3 = new Worker("Wladimir", "Kruz", 1);
@@ -9,5 +8,11 @@ public class Main {
         System.out.println(worker2.getName() + " " + worker2.getSurname() + " з/п: " + worker2.calculateSalary() );
         System.out.println(worker3.getName() + " " + worker3.getSurname() + " з/п: " + worker3.calculateSalary() );
         System.out.println(director1.getName() + " " + director1.getSurname() + " з/п: " + director1.calculateSalary() );
+        //2.Назначить двух созданных сотрудников под управление директора. Распечатать сведения о директоре
+        // (инфо о директоре и об его подчинении).
+        Employee [] workers = new Employee[]{
+                worker1, worker2};
+
+        }
     }
 }
